a68b253961394de683ff2b128460a9d1378b4058
faleevand <faleevandre@gmail.com>
2023-05-25 17:56:33 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.concurrent.Callable;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat catb = (Cat) applicationContext.getBean("cat");
+        Cat catb2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean2);
+        System.out.println(catb == catb2);
     }
+
 }
