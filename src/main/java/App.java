e87918c0e55fdf7ff004875572d26902e49d8012
faleevand <faleevandre@gmail.com>
2023-05-26 23:36:05 +0300

    update1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,6 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.concurrent.Callable;
 
 public class App {
     public static void main(String[] args) {
